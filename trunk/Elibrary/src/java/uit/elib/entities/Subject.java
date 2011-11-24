1e4421d9d51312e604a1086e690078aa3eb4a95d
tiendo.vn <tiendo.vn@36c7b862-18cc-dd17-c128-a3e277489088>
2011-11-24 15:31:49 +0000

    Chagne Form Bean and Hibernate

--- trunk/Elibrary/src/java/uit/elib/entities/Subject.java
+++ trunk/Elibrary/src/java/uit/elib/entities/Subject.java
@@ -1,5 +1,5 @@
 package uit.elib.entities;
-// Generated Nov 24, 2011 12:00:39 AM by Hibernate Tools 3.2.1.GA
+// Generated Nov 24, 2011 9:54:54 PM by Hibernate Tools 3.2.1.GA
 
 
 import java.util.HashSet;
