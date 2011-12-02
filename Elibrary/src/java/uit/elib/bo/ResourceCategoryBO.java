16d9ea8f867effd0e88e167756347752d7d16263
sirhung90 <sirhung90@36c7b862-18cc-dd17-c128-a3e277489088>
2011-12-02 17:43:35 +0000

    Update resourceBO

--- Elibrary/src/java/uit/elib/bo/ResourceCategoryBO.java
+++ Elibrary/src/java/uit/elib/bo/ResourceCategoryBO.java
@@ -34,7 +34,8 @@ public class ResourceCategoryBO extends ManagerBase<Resourcecategory> {
     
      public List getAllResourcecategory() {
         try {
-            String[] sort = new String[]{"resourceCategoryId"}; // tang dan
+            String[] sort = new String[1]; // tang dan
+            sort[0]="resourceCategoryId";
             //String[] sort = new String[]{"name desc"}; // giam dan
 
             List<Resourcecategory> list = getBySQLQuery(sort, 0);
