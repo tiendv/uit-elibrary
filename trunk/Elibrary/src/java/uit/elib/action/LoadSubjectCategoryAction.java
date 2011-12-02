ed4e60999df0bb5dadd66c69312854271d15c98b
sirhung90 <sirhung90@36c7b862-18cc-dd17-c128-a3e277489088>
2011-12-02 06:33:58 +0000

    Update Subject Info Action

--- trunk/Elibrary/src/java/uit/elib/action/LoadSubjectCategoryAction.java
+++ trunk/Elibrary/src/java/uit/elib/action/LoadSubjectCategoryAction.java
@@ -17,20 +17,27 @@ import uit.elib.entities.Resource;
  *
  * @author HERO
  */
-public class LoadInfoSubjectAction extends org.apache.struts.action.Action{
+public class LoadSubjectCategoryAction extends org.apache.struts.action.Action{
     private static final String SUCCESS = "success";
     
     @Override
     public ActionForward execute(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response)
             throws Exception {
-        int subjectID =Integer.parseInt(request.getParameter("SubjectID"));
-        int resourcecategoryID =Integer.parseInt(request.getParameter("ResourceCategoryID"));
+        int subjectID =Integer.parseInt(request.getParameter("subjectID"));
+        int resourcecategoryID =Integer.parseInt(request.getParameter("resourceCategoryID"));
+        int orderChapter = Integer.parseInt(request.getParameter("orderChapter"));
         ResourceBO tempResourceBO = new ResourceBO();
-        List<Resource> listResource = new ArrayList<Resource>();
+        List<Resource> listResource = new ArrayList<Resource>(); // danh sách tài nguyên của
+        List<Resource> listChapter = new ArrayList<Resource>(); // danh sách chương của môn học
         listResource = tempResourceBO.getAllResourceOfSubjectAndResourceCategory(subjectID, resourcecategoryID);
-        //listResource = tempResourceBO.getAllChapterOfSubject(13);
+        listChapter = tempResourceBO.getAllResourceOfSubjectAndResourceCategory(subjectID, 7);
         request.setAttribute("listResource", listResource);
+        request.setAttribute("listChapter", listChapter);
+        request.setAttribute("subjectID", subjectID);
+        request.setAttribute("resourceCategoryID", resourcecategoryID);
+        request.setAttribute("orderChapter", orderChapter);
         return mapping.findForward(SUCCESS);
+        
     }
     }
