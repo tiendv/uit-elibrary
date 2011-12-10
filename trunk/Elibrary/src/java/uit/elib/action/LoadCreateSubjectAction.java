f12c58008acb3943a1a32a366e4696886ca63a1a
nguyenhoangtan2002 <nguyenhoangtan2002@36c7b862-18cc-dd17-c128-a3e277489088>
2011-12-10 10:49:35 +0000

    Thêm mới 2 Action :i LoadCreateResourceAction.java và LoadCreateSubjectAction.java

---
+++ trunk/Elibrary/src/java/uit/elib/action/LoadCreateSubjectAction.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.elib.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import uit.elib.bo.FacultyBO;
+import uit.elib.bo.SubjectCategoryBO;
+import uit.elib.formbean.LoadCreateSubjectForm;
+
+/**
+ *
+ * @author Nguyen Hoang Tan
+ */
+public class LoadCreateSubjectAction extends org.apache.struts.action.Action {
+
+    /* forward name="success" path="" */
+    private static final String SUCCESS = "success";
+
+    /**
+     * This is the action called from the Struts framework.
+     * @param mapping The ActionMapping used to select this instance.
+     * @param form The optional ActionForm bean for this request.
+     * @param request The HTTP Request we are processing.
+     * @param response The HTTP Response we are processing.
+     * @throws java.lang.Exception
+     * @return
+     */
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        LoadCreateSubjectForm loadCreateSubjectForm = (LoadCreateSubjectForm)form;
+        //faculty
+        FacultyBO facultyBO = FacultyBO.getFacultyBO();
+        loadCreateSubjectForm.setListDropFaculty(facultyBO.getAllFaculty());
+        //Subject Category
+        SubjectCategoryBO subjectCategoryBO = SubjectCategoryBO.getSubjectCategoryBO();
+        loadCreateSubjectForm.setListDropSubjectCategory(subjectCategoryBO.getAllSubjectCategory());
+        
+        return mapping.findForward(SUCCESS);
+    }
+}
