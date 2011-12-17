2e6501697117d230ffcc473b134fd954f904172f
sirhung90 <sirhung90@36c7b862-18cc-dd17-c128-a3e277489088>
2011-12-17 19:01:00 +0000

    create new action

---
+++ trunk/Elibrary/src/java/uit/elib/action/LoadListOfSubjectAction.java
@@ -0,0 +1,99 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.elib.action;
+import uit.elib.dto.Subject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import uit.elib.bo.FacultyBO;
+import uit.elib.bo.SubjectBO;
+import uit.elib.bo.SubjectCategoryBO;
+import uit.elib.dto.Faculty;
+import uit.elib.dto.Subjectcategory;
+import uit.elib.formbean.CreateSubjectForm;
+import uit.elib.formbean.LoadCreateSubjectForm;
+import java.util.List;
+/**
+ *
+ * @author HERO
+ */
+public class LoadListOfSubjectAction extends org.apache.struts.action.Action{
+    private static final String SUCCESS = "success";
+
+    /**
+     * This is the action called from the Struts framework.
+     * @param mapping The ActionMapping used to select this instance.
+     * @param form The optional ActionForm bean for this request.
+     * @param request The HTTP Request we are processing.
+     * @param response The HTTP Response we are processing.
+     * @throws java.lang.Exception
+     * @return
+     */
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+          request.setCharacterEncoding("UTF-8");
+          List<Subjectcategory> subjectCategoryBO = SubjectCategoryBO.getSubjectCategoryBO().getAllSubjectCategory();
+          List<Faculty> facultyBO = FacultyBO.getFacultyBO().getAllFaculty();
+            LoadCreateSubjectForm listOfsubjectFormBean = (LoadCreateSubjectForm) form;
+            listOfsubjectFormBean.setListDropSubjectCategory(subjectCategoryBO);
+            listOfsubjectFormBean.setListDropFaculty(facultyBO);
+//        // Get infor category
+//        Subjectcategory tempCategory;
+//        tempCategory = SubjectCategoryBO.getSubjectCategoryBO().getById(subjectFormBean.getDropSubjectCategory(), true);
+//        
+//        // Get infor faculty
+//        Faculty tempFaculty=null;
+//        if(subjectFormBean.getDropSubjectCategory()!=1)
+//            tempFaculty = FacultyBO.getFacultyBO().getById(subjectFormBean.getDropFaculty(), true);
+//                
+//        SubjectBO subjectBO = SubjectBO.getSubjectBO();
+//        Subject temp = new Subject(); 
+//        
+//        temp.setSubjectNameEn(subjectFormBean.getTxtSubjectNameUS());
+//        temp.setSubjectNameVn(subjectFormBean.getTxtSubjectName());
+//        temp.setSubjectcategory(tempCategory);
+//        temp.setCourseCode(subjectFormBean.getTxtSubjectCode());
+//        temp.setPeriodOfTheory(Integer.parseInt(subjectFormBean.getTxtPeriodOfTheory()));
+//        temp.setPeriodOfPractice(Integer.parseInt(subjectFormBean.getTxtPeriodOfPractice()));
+//        temp.setMidtermGrade(Integer.parseInt(subjectFormBean.getTxtMidtermGrade()));
+//        temp.setFinalGrade(Integer.parseInt(subjectFormBean.getTxtFinalGrade()));
+//        temp.setPrerequisiteSubjectVn(subjectFormBean.getTxtPrerequisiteSubjectVN());
+//        temp.setPrerequisiteSubjectEn(subjectFormBean.getTxtPrerequisiteSubject());
+//        temp.setTimeTeaching(String.valueOf(subjectFormBean.getTxtTimeTeaching()));
+//        temp.setTeacher(subjectFormBean.getTxtTeacherName());
+//        temp.setLevel(subjectFormBean.getTxtLevel());
+//        temp.setFaculty(tempFaculty);
+//        temp.setSubjectIntroduceVn(subjectFormBean.getFckintroductionVN());
+//        temp.setSubjectIntroduceEn(subjectFormBean.getFckintroductionUS());
+//        temp.setProjectRequirementVn(subjectFormBean.getFckProjectRequitementVN());
+//        temp.setProjectRequirementEn(subjectFormBean.getFckProjectRequitementUS());
+//        temp.setNumberChapter(Integer.parseInt(subjectFormBean.getTxtNumberChapter()));
+//        temp.setNumberOfCredit(Integer.parseInt(subjectFormBean.getTxtCreditNumber()));
+//        subjectBO.addNew(temp);
+//        //reset form
+//        subjectFormBean.setTxtSubjectNameUS("");
+//        subjectFormBean.setTxtSubjectName("");
+//        subjectFormBean.setDropSubjectCategory(0);
+//        subjectFormBean.setDropFaculty(-1);
+//        subjectFormBean.setTxtCreditNumber("");
+//        subjectFormBean.setTxtSubjectCode("");
+//        subjectFormBean.setTxtPeriodOfTheory("");
+//        subjectFormBean.setTxtPeriodOfPractice("");
+//        subjectFormBean.setTxtMidtermGrade("");
+//        subjectFormBean.setTxtFinalGrade("");
+//        subjectFormBean.setTxtPrerequisiteSubject("");
+//        subjectFormBean.setTxtPrerequisiteSubjectVN("");
+//        subjectFormBean.setTxtTimeTeaching("");
+//        subjectFormBean.setTxtTeacherName("");
+//        subjectFormBean.setTxtLevel("");
+//        subjectFormBean.setTxtNumberChapter("");
+        
+        return mapping.findForward(SUCCESS);
+    }
+}
---
+++ trunk/Elibrary/src/java/uit/elib/action/LoadListSubjectOfCategoryAction.java
@@ -0,0 +1,115 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.elib.action;
+
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import uit.elib.bo.SubjectBO;
+import uit.elib.dto.Subject;
+/**
+ *
+ * @author HERO
+ */
+public class LoadListSubjectOfCategoryAction extends org.apache.struts.action.Action{
+    
+    private static final String SUCCESS = "success";
+    
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        response.setCharacterEncoding("UTF-8");
+        String where="";
+        List<Subject> listSubject;
+        String[] sort = new String[]{"subjectId"};
+        if(request.getParameter("subjectCategoryID")!= null && request.getParameter("facultyID")!= null ) // if both of them is valid
+        {
+            int color=1;
+            int categoryID = Integer.parseInt(request.getParameter("subjectCategoryID"));
+            int facultyID = Integer.parseInt(request.getParameter("facultyID"));
+            int language = Integer.parseInt(request.getParameter("language")); // 1-EN, 2-VI
+            if(facultyID== -1)
+                where = "subjectCategoryId="+categoryID;
+            else
+                where = "subjectCategoryId="+categoryID +" and facultyId="+facultyID;
+            //sort[0]="subjectId";
+            listSubject = SubjectBO.getSubjectBO().getAllSubject(where, sort);
+            
+            
+            //begin visible TABLE
+            response.getWriter().println("<table class=resource_table>");
+                response.getWriter().println("<tr class=color_title_table>");//Title of table
+                response.getWriter().println("<td width=25%>");
+                if(language==1) //EN
+                    response.getWriter().println("Subject code");
+                if(language==2) //VI
+                    response.getWriter().println("Mã môn học");
+                response.getWriter().println("</td>");
+                response.getWriter().println("<td width=60%>");
+                if(language==1) //EN
+                    response.getWriter().println("Subject name");
+                if(language==2) //VI
+                    response.getWriter().println("Tên môn học");
+                response.getWriter().println("</td>");
+                response.getWriter().println("<td width=15%>");
+                if(language==1) //EN
+                    response.getWriter().println("Delete");
+                if(language==2) //VI
+                    response.getWriter().println("Xóa");
+                response.getWriter().println("</td>");
+                response.getWriter().println("</tr>");
+            if(listSubject.size()==0)
+            {
+                response.getWriter().println("<tr>");
+                response.getWriter().println("<td>");
+                response.getWriter().println("</td>");
+                response.getWriter().println("<td>");
+                if(language==1) //EN
+                    response.getWriter().println("None");
+                if(language==2) //VI
+                    response.getWriter().println("Không có");
+                response.getWriter().println("</td>");
+                response.getWriter().println("</tr>");
+            }
+            else
+            {
+                
+                for(int i=0; i<listSubject.size();i++)
+                {
+                    response.getWriter().println("<tr>");
+                    if(color%2==0)//thay đổi màu xen kẽ cho dòng
+                        response.getWriter().println("<td class=color_table2>");
+                    else
+                    response.getWriter().println("<td>");
+                    response.getWriter().println(listSubject.get(i).getCourseCode().toString());
+                    response.getWriter().println("</td>");
+                    if(color%2==0)//thay đổi màu xen kẽ cho dòng
+                        response.getWriter().println("<td class=color_table2>");
+                    else
+                        response.getWriter().println("<td>");
+                    if(language==1)
+                    response.getWriter().println(listSubject.get(i).getSubjectNameEn().toString());
+                    if(language==2)
+                    response.getWriter().println(listSubject.get(i).getSubjectNameVn().toString());
+                    response.getWriter().println("</td>");
+                    if(color%2==0)//thay đổi màu xen kẽ cho dòng
+                        response.getWriter().println("<td class=color_table2>");
+                    else
+                        response.getWriter().println("<td>");
+                    response.getWriter().println("<input type=checkbox name=ckb value=ON/>");
+                    response.getWriter().println("</td>");        
+                    response.getWriter().println("</tr>");
+                    color++;
+                }
+                response.getWriter().println("</table>");
+            }
+        }
+        return mapping.findForward(SUCCESS);
+    }
+}
