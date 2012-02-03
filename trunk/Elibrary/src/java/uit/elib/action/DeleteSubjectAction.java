04816b557bda58f6ba7f1b83b0d9c65d42ef2a8e
sirhung90 <sirhung90@36c7b862-18cc-dd17-c128-a3e277489088>
2012-02-03 18:30:29 +0000

    update delete resource of subject

--- trunk/Elibrary/src/java/uit/elib/action/DeleteSubjectAction.java
+++ trunk/Elibrary/src/java/uit/elib/action/DeleteSubjectAction.java
@@ -4,6 +4,8 @@
  */
 package uit.elib.action;
 
+import java.io.File;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.ActionForm;
@@ -11,6 +13,7 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import uit.elib.bo.ResourceBO;
 import uit.elib.bo.SubjectBO;
+import uit.elib.dto.Resource;
 /**
  *
  * @author HERO
@@ -36,10 +39,21 @@ public class DeleteSubjectAction extends org.apache.struts.action.Action {
         subjectsID = subjectsID.substring(0, subjectsID.length()-1);
         String sqlresource = "delete from resource where SubjectID in("+subjectsID+")";
         String sqlsubject = "delete from subject where SubjectID in("+subjectsID+")";
+        List<Resource> listresource = ResourceBO.getResourceBO().getAllResource("SubjectID in("+subjectsID+")", null);
 //        ResourceBO resourceBO = ResourceBO.getResourceBO();
 //        resourceBO.DeleteResource(sqlresource);
 //        SubjectBO subjectBO = SubjectBO.getSubjectBO();
 //        subjectBO.DeleteSubject(sqlsubject);
+        for (int i = 0; i < listresource.size(); i++) 
+        {
+            if(listresource.get(i).getServerName()!=null)
+            {
+                String filePath = getServlet().getServletContext().getRealPath("/") +"upload/"+listresource.get(i).getServerName();
+                File file = new File(filePath);
+                file.delete();
+            }
+        }
+        
         ResourceBO.getResourceBO().DeleteResource(sqlresource);
         SubjectBO.getSubjectBO().DeleteSubject(sqlsubject);
         return mapping.findForward(null);
