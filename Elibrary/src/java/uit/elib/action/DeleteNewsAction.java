403aafc42bb3a21e24ef056218742d79d38b5ddf
nguyenhoangtan2002 <nguyenhoangtan2002@36c7b862-18cc-dd17-c128-a3e277489088>
2012-02-03 09:42:20 +0000

    update

--- Elibrary/src/java/uit/elib/action/DeleteNewsAction.java
+++ Elibrary/src/java/uit/elib/action/DeleteNewsAction.java
@@ -4,12 +4,15 @@
  */
 package uit.elib.action;
 
+import java.io.File;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import uit.elib.bo.NewsBO;
+import uit.elib.dto.News;
 
 /**
  *
@@ -32,7 +35,17 @@ public class DeleteNewsAction extends org.apache.struts.action.Action {
             throws Exception {
         String newsID = request.getParameter("newsID");
         newsID=newsID.substring(0,newsID.length()-1);
-        NewsBO.getNewsBO().DeleteNews("delete from news where newsID in("+newsID+")");
+        List<News> listNews = NewsBO.getNewsBO().getAllNews("NewsID in("+newsID+")",null );
+        for(int i=0;i<listNews.size();i++)
+        {    
+            // delete old image
+            if(listNews.get(i).getNewsImage()!=null)
+            {
+                File oldFile =  new File(request.getServletContext().getRealPath("/")+"upload/"+listNews.get(i).getNewsImage());
+                oldFile.delete();
+            }                
+            NewsBO.getNewsBO().DeleteNews("delete from news where newsID ="+listNews.get(i).getNewsId()) ;
+        }        
         return null;
     }
 }
