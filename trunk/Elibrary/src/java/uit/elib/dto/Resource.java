294db57aeb18869bbb761111f6c5f6835700cad0
nguyenhoangtan2002 <nguyenhoangtan2002@36c7b862-18cc-dd17-c128-a3e277489088>
2012-02-17 16:56:50 +0000

    update Elibrary-17-02-2012_23g50 + dto + hibernate.cfg.xml + hibernate.reveng.xml

--- trunk/Elibrary/src/java/uit/elib/dto/Resource.java
+++ trunk/Elibrary/src/java/uit/elib/dto/Resource.java
@@ -1,5 +1,5 @@
 package uit.elib.dto;
-// Generated Feb 12, 2012 10:39:26 PM by Hibernate Tools 3.2.1.GA
+// Generated Feb 17, 2012 11:57:50 PM by Hibernate Tools 3.2.1.GA
 
 
 import java.util.Date;
